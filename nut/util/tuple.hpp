793c2fd7c2e4071234f1b8a7595de4de8387c8a0
jingqi <jingqi_se@hotmail.com>
2012-01-31 19:42:29 +0800

    增加tuple的参数个数

--- nut/util/tuple.hpp
+++ nut/util/tuple.hpp
@@ -11,7 +11,8 @@
 namespace nut
 {
 
-template <typename T1, typename T2, typename T3 = void, typename T4 = void, typename T5 = void>
+template <typename T1, typename T2, typename T3 = void, typename T4 = void, typename T5 = void,
+          typename T6 = void>
 struct Tuple
 {
     T1 first;
@@ -19,30 +20,73 @@ struct Tuple
     T3 third;
     T4 forth;
     T5 fifth;
+    T6 sixth;
+
+    Tuple() {}
+
+    Tuple(const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5,
+          const T6& t6)
+        : first(t1), second(t2), third(t3), forth(t4), fifth(t5),
+          sixth(t6)
+    {}
 };
 
-template <typename T1, typename T2, typename T3, typename T4>
-struct Tuple<T1, T2, T3, T4, void>
+template <typename T1, typename T2, typename T3, typename T4, typename T5>
+struct Tuple<T1, T2, T3, T4, T5, void>
 {
     T1 first;
     T2 second;
     T3 third;
     T4 forth;
+    T5 fifth;
+
+    Tuple() {}
+
+    Tuple(const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5)
+        : first(t1), second(t2), third(t3), forth(t4), fifth(t5)
+    {}
 };
 
-template <typename T1, typename T2, typename T3>
-struct Tuple<T1, T2, T3, void, void>
+template <typename T1, typename T2, typename T3, typename T4>
+struct Tuple<T1, T2, T3, T4, void, void>
 {
     T1 first;
     T2 second;
     T3 third;
+    T4 forth;
+
+    Tuple() {}
+
+    Tuple(const T1& t1, const T2& t2, const T3& t3, const T4& t4)
+        : first(t1), second(t2), third(t3), forth(t4)
+    {}
 };
 
-template <typename T1, typename T2>
-struct Tuple<T1, T2, void, void, void>
+template <typename T1, typename T2, typename T3>
+struct Tuple<T1, T2, T3, void, void, void>
 {
     T1 first;
     T2 second;
+    T3 third;
+
+    Tuple() {}
+
+    Tuple(const T1& t1, const T2& t2, const T3& t3)
+        : first(t1), second(t2), third(t3)
+    {}
+};
+
+template <typename T1, typename T2>
+struct Tuple<T1, T2, void, void, void, void>
+{
+    T1 first;
+    T2 second;
+
+    Tuple() {}
+
+    Tuple(const T1& t1, const T2& t2)
+        : first(t1), second(t2)
+    {}
 };
 
 }
