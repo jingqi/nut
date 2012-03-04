b801bbe1abc0beb945b0b6ecae12a15c28b49ecc
jingqi <jingqi_se@hotmail.com>
2012-03-04 20:35:57 +0800

    并发无锁队列

---
+++ nut/concurrent/ConcurrentQueue.hpp
@@ -0,0 +1,24 @@
+﻿/**
+ * @file -
+ * @author jingqi
+ * @date 2012-03-04
+ * @last-edit 2012-03-04 20:32:25 jingqi
+ */
+
+#ifndef ___HEADFILE_C020D343_98AA_41A4_AFE8_01825671348C_
+#define ___HEADFILE_C020D343_98AA_41A4_AFE8_01825671348C_
+
+namespace nut
+{
+
+/**
+ * 无锁并发队列
+ */
+class ConcurentQueue
+{};
+
+}
+
+#endif
+
+
