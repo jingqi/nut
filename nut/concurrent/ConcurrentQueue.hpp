00d53f018428f18414f2f7cc802773f15125098f
jingqi <jingqi_se@hotmail.com>
2012-03-05 21:19:06 +0800

    添加CAS操作的实现

--- nut/concurrent/ConcurrentQueue.hpp
+++ nut/concurrent/ConcurrentQueue.hpp
@@ -2,20 +2,120 @@
  * @file -
  * @author jingqi
  * @date 2012-03-04
- * @last-edit 2012-03-04 20:32:25 jingqi
+ * @last-edit 2012-03-05 18:27:26 jingqi
  */
 
 #ifndef ___HEADFILE_C020D343_98AA_41A4_AFE8_01825671348C_
 #define ___HEADFILE_C020D343_98AA_41A4_AFE8_01825671348C_
+
+#include <nut/platform/platform.hpp>
+
+#include "atomic.hpp"
 
 namespace nut
 {
 
 /**
  * 无锁并发队列
+ *
+ * 参考资料和关键字：
+ *    MS-queue(Michael and Scott)算法, Compare-and-swap(CAS)操作,
+ *    Dominique Fober算法, optimistic算法, 消隐(shavit and Touitou)
  */
+template <typename T>
 class ConcurentQueue
-{};
+{
+#if defined(NUT_PLATFORM_BITS_64)
+    typedef uint128_t cas_type;
+#elif defined(NUT_PLATFORM_BITS_32)
+    typedef uint64_t cas_type;
+#elif defined(NUT_PLATFORM_BITS_16)
+    typedef uint32_t cas_type;
+#endif
+
+    template <typename U>
+    union CASPtr
+    {
+        typedef struct
+        {
+            U *ptr;
+            unsigned int tag;
+        } uptr;
+        cas_type cas;
+
+        CASPtr(U *p = NULL, unsigned int t = 0) : ptr(p), tag(t) {}
+    };
+    static_assert(sizeof(CASPtr::uptr) == sizeof(cas_type));
+
+    struct Node
+    {
+        T data;
+        CASPtr<Node> pre;
+        CASPtr<Node> next;
+
+        Node(T& v) : data(v) {}
+    };
+    
+    CASPtr<Node> volatile m_head;
+    CASPtr<Node> volatile m_tail;
+
+public:
+    ConcurentQueue()
+    {
+        Node *dumy = (Node*) ::malloc(sizeof(Node));
+        dumy->next.cas = 0;
+        dumy->pre.cas = 0;
+        m_head.ptr = dumy;
+        m_tail.ptr = dumy;
+    }
+
+    ~ConcurentQueue()
+    {
+        clear();
+        ::free(m_head.ptr);
+        m_head.cas = 0;
+        m_end.cas = 0;
+    }
+
+public:
+    void enquere(Node *new_node)
+    {
+        while (true)
+        {
+            // read the tail
+            CASPtr oldTail(m_tail);
+
+            // set node's next ptr
+            new_node->next.ptr = oldTail.ptr;
+            new_node->next.tag = oldTail.tag + 1;
+
+            // try CAS the tail
+            CASPtr newNode(new_node, oldTail.tag + 1);
+            if (atomic_cas(&(m_tail.cas), oldTail.cas, newNode.cas)) {
+                oldTail.ptr->pre.ptr = new_node;
+                oldTail.ptr->pre.tag = oldTail.tag;
+                break;
+            }
+        }
+    }
+
+    Node* dequeue()
+    {
+        // TODO
+    }
+
+private:
+    /** CAS 操作 */
+    static inline bool atomicCAS(long *ptr, long oldval, long newval)
+    {
+#if defined(NUT_PLATFORM_CC_GCC)
+        return __sync_val_compare_and_swap(ptr, oldval, newval);
+#elif defined(NUT_PLATFORM_CC_VC)
+        return InterlockedCompareExchange(ptr, oldval, newval) == oldval;
+        InterlockedCompareExchange64
+#endif
+    }
+};
 
 }
 
---
+++ nut/concurrent/concurrentstack.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file -
+ * @author jingqi
+ * @date 2012-03-05
+ * @last-edit 2012-03-05 21:18:16 jingqi
+ */
+
+#ifndef ___HEADFILE_039EC871_866B_4C6A_AF26_747D92A9ADA7_
+#define ___HEADFILE_039EC871_866B_4C6A_AF26_747D92A9ADA7_
+
+namespace nut
+{
+
+template <typename T>
+{};
+
+}
+
+#endif
+
+
