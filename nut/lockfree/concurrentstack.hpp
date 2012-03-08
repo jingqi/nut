11ffd7f4319516a9344c770c5ed34b0d0929962e
jingqi <jingqi_se@hotmail.com>
2012-03-08 20:15:22 +0800

    完善 lock-free stack

--- nut/lockfree/concurrentstack.hpp
+++ nut/lockfree/concurrentstack.hpp
@@ -14,7 +14,7 @@
 namespace nut
 {
 
-template <typename T, typename AllocT = std::allocate>
+template <typename T, typename AllocT = std::allocator<T> >
 class ConcurrentStack
 {
     struct Node
@@ -35,6 +35,7 @@ class ConcurrentStack
 
 public:
     ConcurrentStack()
+        : m_size(0)
     {}
 
     void push(const T& v)
@@ -42,20 +43,39 @@ public:
         Node *n = m_nodeAlloc.allocate(1);
         m_dataAlloc.construct(&(n->data), v);
         push(n);
+        atomic_add(m_size, 1);
     }
 
+    bool pop(T *p)
+    {
+        assert(NULL != p);
+        Node *pn = pop();
+        if (NULL == pn)
+            return false;
+
+        *p = pn->data;
+        m_dataAlloc.destroy(&pn->data);
+        m_nodeAlloc.deallocate(pn, 1);
+        atomic_add(m_size, -1);
+        return true;
+    }
+
+    size_t size()
+    {
+        return m_size;
+    }
+
+private:
     void push(Node *new_node)
     {
+        assert(NULL != new_node);
         while (true)
         {
-            TagedPtr<Node> *oldTop = m_top;
+            TagedPtr<Node> oldTop = m_top;
             new_node->next = oldTop.ptr;
             TagedPtr<Node> *newTop(new_node, oldTop.tag + 1);
             if (atomic_cas(&m_top, oldTop, newTop))
-            {
-                atomic_add(m_size, 1);
                 return;
-            }
         }
     }
 
@@ -68,21 +88,10 @@ public:
             if (NULL == oldTop.ptr)
                 return NULL;
 
-            TagedPtr<Node> topNodeNext = oldTop.ptr->next;
-
-            TagedPtr<Node> newTop(oldTop.ptr->next);
-            Node *next = oldTop->next;
-            if (atomic_cas(&m_top, oldTop, next))
-            {
-                atomic_add(m_size, -1);
-                return oldTop->data;
-            }
+            TagedPtr<Node> newTop(oldTop.ptr->next, oldTop.tag + 1);
+            if (atomic_cas(&m_top, oldTop, newTop))
+                return oldTop.ptr;
         }
-    }
-
-    size_t size()
-    {
-        return m_size;
     }
 };
 
