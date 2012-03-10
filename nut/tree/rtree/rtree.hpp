bdc16405c344f190feaf8086a551d21f0c3bf387
jingqi <jingqi_se@hotmail.com>
2012-03-10 15:41:46 +0800

    rtree框架

---
+++ nut/tree/rtree/rtree.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file -
+ * @author jingqi
+ * @date 2012-03-10
+ * @last-edit 2012-03-10 15:35:56 jingqi
+ */
+
+#ifndef ___HEADFILE_160547E9_5A30_4A78_A5FF_76E0C5EBE229_
+#define ___HEADFILE_160547E9_5A30_4A78_A5FF_76E0C5EBE229_
+
+namespace nut
+{
+
+template <typename T>
+class RTree
+{
+};
+
+}
+
+#endif /* head file guarder */
+
+
---
+++ nut/tree/rtree/xtree.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file -
+ * @author jingqi
+ * @date 2012-03-10
+ * @last-edit 2012-03-10 15:36:42 jingqi
+ */
+
+#ifndef ___HEADFILE_160547E9_5A30_4A78_A5FF_76E0C5EBE229_
+#define ___HEADFILE_160547E9_5A30_4A78_A5FF_76E0C5EBE229_
+
+namespace nut
+{
+
+template <typename T>
+class XTree
+{
+};
+
+}
+
+#endif /* head file guarder */
+
+
