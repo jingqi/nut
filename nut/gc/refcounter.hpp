d10d0605f155b93edf918f3609362780dc3f6d3f
jingqi <jingqi_se@hotmail.com>
2012-04-01 21:09:40 +0800

    将原子操作应用于多线程下的引用计数中

--- nut/gc/refcounter.hpp
+++ nut/gc/refcounter.hpp
@@ -9,6 +9,8 @@
 #ifndef ___HEADFILE___9D7DF0EC_0FB5_48E2_8771_6F52D286DE11_
 #define ___HEADFILE___9D7DF0EC_0FB5_48E2_8771_6F52D286DE11_
 
+#include <nut/lockfree/atomic.hpp>
+
 namespace nut
 {
 
@@ -32,8 +34,16 @@ struct RefCounterSync
     volatile int m_value;
 
     RefCounterSync(int v) : m_value(v) {}
-    inline int operator++ () { return ++m_value; }
-    inline int operator-- () { return --m_value; }
+
+    inline int operator++ ()
+    {
+        return atomic_add(&m_value, 1) + 1;
+    }
+
+    inline int operator-- ()
+    {
+        return atomic_add(&m_value, -1) - 1;
+    }
 };
 
 }
