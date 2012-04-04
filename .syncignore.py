9428fd897a151300e3faffd9af2a260633274c04
jingqi <jingqi_se@hotmail.com>
2012-04-04 23:08:24 +0800

    修改syncignore配置项

--- .syncignore.py
+++ .syncignore.py
@@ -7,11 +7,16 @@ ignore_names = (
     re.compile('.*\\.user$', re.IGNORECASE),
     re.compile('.*\\.opensdf$', re.IGNORECASE),
     re.compile('.*\\.sdf$', re.IGNORECASE),
+    re.compile('.*\\.pyc$', re.IGNORECASE),
 )
 
 ignore_paths = (
     'wks/MSVC/Debug',
+    'wks/MSVC/Release',
     'wks/MSVC/ipch',
     'wks/MSVC/MSVC/Debug',
+    'wks/MSVC/MSVC/Release',
     'tmp',
+	'wks/makefile/makefile',
+	'wks/makefile/output',
 )
