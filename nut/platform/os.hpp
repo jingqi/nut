3f21f1e7943499675e33b475825e384f3d7bd580
jingqi <jingqi_se@hotmail.com>
2012-06-23 17:26:31 +0800

    添加部分平台相关的路径、文件等的处理

---
+++ nut/platform/os.hpp
@@ -0,0 +1,142 @@
+﻿/**
+ * @file -
+ * @author jingqi
+ * @date 2012-06-23
+ * @last-edit 2012-06-23 15:41:27 jingqi
+ */
+
+#ifndef ___HEADFILE_291DFB4C_7D29_4D61_A691_EF83FB86CD36_
+#define ___HEADFILE_291DFB4C_7D29_4D61_A691_EF83FB86CD36_
+
+#include <assert.h>
+
+#include "platform.hpp"
+
+#if defined(NUT_PLATFORM_OS_WINDOWS)
+#   include <windows.h>
+#else
+#   include <dirent.h>  // for DIR, dirent
+#   include <limits.h>   // for PATH_MAX
+#   include <sys/stat.h> // for lstat()
+#   include <stdlib.h>
+#endif
+
+namespace nut
+{
+
+class OS
+{
+private:
+    OS() {}
+
+public:
+
+    /**
+     * 枚举目录下的文件/文件夹
+     *
+     * @param except_file 如果传入true, 则返回值不会包含文件
+     * @parma except_dir 如果传入true, 则返回值不会包含文件夹
+     * @parma except_initial_dot 如果传入true, 则返回值不会包含以'.'开头的文件/文件夹
+     */
+    static std::vector<std::string> listdir(const char *path, bool except_file = false,
+            bool except_dir = false, bool except_initial_dot = false)
+    {
+        assert(NULL != path);
+        std::vector<std::string> ret;
+
+#if defined(NUT_PLATFORM_OS_WINDOWS)
+        char search_path[MAX_PATH];
+        ::sprintf(search_path, "%s\\*", path); /* 加上通配符 */
+
+        WIN32_FIND_DATAA wfd;
+        HANDLE hFind = ::FindFirstFileA(search_path, &wfd);
+        if (hFind == INVALID_HANDLE_VALUE)
+            return ret;
+
+        do
+        {
+            if (except_initial_dot && wfd.cFileName[0] == '.')
+                continue;
+            if (except_file && !(wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
+                continue;
+            if (except_dir && (wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
+                continue;
+
+            ret.push_back(wfd.cFileName);
+        } while (::FindNextFileA(hFind, &wfd));
+
+        // 关闭查找句柄
+        ::FindClose(hFind);
+        return ret;
+#else
+        DIR *dp = NULL;
+        struct dirent *dirp = NULL;
+        if ((dp = ::opendir(path)) == NULL)
+            return ret;
+
+        while ((dirp = ::readdir(dp)) != NULL)
+        {
+            if (except_initial_dot && dirp->d_name[0] == '.')
+                continue;
+
+            if (except_file || except_dir)
+            {
+                char file_path[PATH_MAX];
+                ::sprintf(file_path, "%s/%s", path, dirp->d_name);
+                struct stat buf;
+                if (::lstat(file_path, &buf) < 0)
+                    continue;
+                if (except_file && !S_ISDIR(buf.st_mode))
+                    continue;
+                if (except_dir && S_ISDIR(buf.st_mode))
+                    continue;
+            }
+
+            ret.push_back(dirp->d_name);
+        }
+
+        // 释放DIR (struct dirent是由DIR维护的，无需额外释放)
+        ::closedir(dp);
+        return ret;
+#endif
+    }
+
+    /**
+     * 复制文件
+     */
+    static bool copyFile(const char *src, const char *dest)
+    {
+        assert(NULL != src && NULL != dest);
+
+#if defined(NUT_PLATFORM_OS_WINDOWS)
+        return FALSE != ::CopyFileA(src, dest, TRUE);
+#else
+        FILE *inFile = ::fopen(src, "rb");
+        if (NULL == inFile)
+            return false;
+            
+        FILE *outFile = ::fopen(dest, "wb+");
+        if (NULL == outFile)
+        {
+            ::fclose(inFIle);
+            return false;
+        }
+        
+        const int BUF_LEN = 4096;
+        char buf[BUF_LEN];
+        int readed = -1;
+        while ((readed = ::fread(buf, BUF_LEN, 1, inFile)) > 0)
+        {
+            ::fwrite(buf, readed, 1, outFile);
+        }
+        ::fclose(inFile);
+        ::fclose(outFile);
+        return true;
+#endif
+    }
+};
+
+}
+
+#endif
+
---
+++ nut/platform/path.hpp
@@ -0,0 +1,221 @@
+﻿/**
+ * @file -
+ * @author jingqi
+ * @date 2012-06-23
+ * @last-edit 2012-06-23 15:38:33 jingqi
+ */
+
+#ifndef ___HEADFILE_E6D40B10_E5D6_4092_A38B_4E69E5B8E123_
+#define ___HEADFILE_E6D40B10_E5D6_4092_A38B_4E69E5B8E123_
+
+#include <assert.h>
+#include <string>
+#include <vector>
+#include <unistd.h> // for access()
+
+#include "platform.hpp"
+
+#if defined(NUT_PLATFORM_OS_WINDOWS)
+#   include <windows.h>
+#endif
+
+
+namespace nut
+{
+
+class Path
+{
+private:
+    Path() {}
+
+public:
+    /**
+     * 路径分隔符
+     */
+    static inline char seperator()
+    {
+#if defined(NUT_PLATFORM_OS_WINDOWS
+        return '\\';
+#else
+        return '/';
+#endif
+    }
+
+    /**
+     * 检查字符是否是路径分隔符 '\\' '/'
+     */
+    static inline bool isPathSeparator(char c)
+    {
+        return '\\' == c || '/' == c;
+    }
+
+    static std::string abspath(const std::string& p) {}
+    static std::string realpath() {}
+    static std::string relpath() {}
+
+    static std::string sep() {}
+
+    /**
+     * 从路径中划分出父路径和 文件/文件夹 名
+     *
+     * 例如：
+     * "ab/c/d" -> "ab/c" "d"
+     * "/ab.txt" -> "/" "ab.txt"
+     * "c:\\tmp" -> "c:\\" "tmp"
+     */
+    static Tuple<std::string,std::string> split(const char *path)
+    {
+        assert(NULL != path);
+        
+        // 找到最后一个 '/'
+        const std::string p(path);
+        std::string::size_type p1 = p.last_index_of('\\'), p2 = p.last_index_of('/');
+        if (std::string::npos == p1 && std::string::npos == p2)
+            return Tuple<std::string, std::string>("", p);
+        else if (std::string::npos != p1 && std::string::npos != p2)
+            p1 = (p1 > p2 ? p1 : p2);
+        else if (std::string::npos == p1)
+            p1 = p2;
+
+        assert(p1 >= 0 && std::string::npos != p1);
+        if (0 == p1 || ':' == path[p1 - 1]) // 磁盘号 + 根目录
+            return Tuple<std::string, std::string>(p.substr(0, p1 + 1), p.substr(p1 + 1));
+        else
+            return Tuple<std::string, std::string>(p.substr(0, p1), p.substr(p1 + 1));
+    }
+
+    /**
+     * 从路径中划分出磁盘号和路径(linux路径的磁盘号假定为"")
+     *
+     * 例如：
+     * "c:\\mn\\p" -> "c:" "\\mn\\p"
+     * "/mnt/sdcard" -> "" "/mnt/sdcard"
+     */
+    static Tuple<std::string,std::string> splitdrive(const char *path)
+    {
+        assert(NULL != path);
+        int pos = -1;
+        for (int i = 0; '\0' != path[i]; ++i)
+        {
+            if ('\\' == path[i] || '/' == path[i])
+                break;
+            if (':' == path[i])
+            {
+                pos = i;
+                break;
+            }
+        }
+        if (pos < 0)
+            return Tuple<std::string,std::string>("", path);
+        const std::string p(path);
+        return Tuple<std::string,std::string>(p.substr(0, pos + 1), p.substr(pos + 1));
+    }
+
+    /**
+     * 从路径或者文件名中分离出后缀名
+     *
+     * 例如：
+     * "a.txt" -> "a" ".txt"
+     */
+    static Tuple<std::string,std::string> splitext(const char *path)
+    {
+        assert(NULL != path);
+        const std::string p(path);
+        int pos = -1;
+        for (int i = p.length() - 1; i >= 0; --i)
+        {
+            if ('\\' == path[i] || '/' == path[i])
+                break;
+            if ('.' == path[i])
+            {
+                // 排除 "a/.abc" 的情况
+                if (i - 1 >= 0 && '\\' != path[i - 1] && '/' != path[i - 1])
+                    pos = i;
+                break;
+            }
+        }
+        if (pos < 0)
+            return Tuple<std::string,std::string>(p,"");
+        return Tuple<std::string,std::string>(p.substr(0,pos),p.substr(pos));
+    }
+
+    static void splitunc() {}
+
+    /**
+     * 检查路径是否存在
+     */
+    static inline bool exists(const char *path)
+    {
+        assert(NULL != path);
+        /*
+         *  0-检查文件是否存在
+         *  1-检查文件是否可运行
+         *  2-检查文件是否可写访问
+         *  4-检查文件是否可读访问
+         *  6-检查文件是否可读/写访问
+         */
+        return 0 == ::access(path, 0);
+    }
+
+    /**
+     * last access time
+     */
+    static time_t getatime()
+    {}
+
+    /**
+     * last modified time
+     */
+    static time_t getmtime() {}
+
+    /**
+     * created time
+     */
+    static time_t getctime() {}
+
+    static long getsize() {}
+
+    static bool isabs() {}
+    static bool isdir() {}
+    static bool isfile() {}
+    static bool islink() {}
+    static bool ismount() {}
+
+    /**
+     * 连接两个子路径
+     *
+     * 例如：
+     * "a" "b" -> "a/b"
+     * "/" "sd" -> "/sd"
+     * "c:" "\\tmp" -> "c:\\tmp"
+     */
+    static std::string join(const char *a, const char *b)
+    {
+        assert(NULL != a && NULL != b);
+
+        if ('\0' == a[0])
+            return b;
+            
+        // 处理磁盘号
+        std::string apath(a);
+        if (':' == a[apath.length() - 1])
+            return apath + b;
+
+        // 处理根目录 '/', 'c:\\'
+        std::string bpath(b);
+        if ('\\' == b[0] || '/' == b[0] || std::string::npos != b.indexof(':'))
+            return bpath;
+
+        // 连接
+        if ('\\' != *apath.rbegin() && '/' != *apath.rbegin())
+            apath.append(seperator());
+        if ('\0' != b[0])
+            apath.append(bpath);
+        return apath;
+    }
+};
+
+}
+
+#endif
+
