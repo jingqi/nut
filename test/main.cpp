82fd65d8a0d3bfddb51d4d8efa02a4076a6c028e
jingqi <jingqi_se@hotmail.com>
2012-07-07 02:55:28 +0800

    添加MD5消息摘要

--- test/main.cpp
+++ test/main.cpp
@@ -21,7 +21,7 @@ int main()
 
     ConsoleTestLogger l;
     TestRunner trunner(l);
-    trunner.runAGroup("mem");
+    trunner.runAGroup("security");
     // trunner.runAFixture("TestGC");
     // trunner.runACase("TestGC", "testSmoking");
 
