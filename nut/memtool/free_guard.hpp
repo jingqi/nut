f649afb90bfbe83b02ab57616d798c4c1e845a92
jingqi <jingqi_se@hotmail.com>
2013-09-15 22:25:43 +0800

    添加自动调用 ::free() 的工具

---
+++ nut/memtool/free_guard.hpp
@@ -0,0 +1,38 @@
+/**
+ * @file -
+ * @author jingqi
+ * @date 2013-09-15
+ * @last-edit 2013-09-15 22:24:44 jingqi
+ * @brief
+ */
+
+#ifndef ___HEADFILE_49E4EA4F_3D1E_4E4E_A406_0186EBB688D9_
+#define ___HEADFILE_49E4EA4F_3D1E_4E4E_A406_0186EBB688D9_
+
+#include <assert.h>
+#include <stdlib.h>
+
+namespace nut
+{
+
+class FreeGuard
+{
+    void *m_ptr;
+public:
+    FreeGuard(void *p)
+        : m_ptr(p)
+    {
+        assert(NULL != p);
+    }
+
+    ~FreeGuard()
+    {
+        assert(NULL != m_ptr);
+        ::free(m_ptr);
+        m_ptr = NULL;
+    }
+};
+
+};
+
+#endif
