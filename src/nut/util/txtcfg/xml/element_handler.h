244e05a5c9db57ebf78ba43dd4398fecda34bbee
jingqi <jingqi.se@gmail.com>
2014-09-03 00:15:17 +0800

    添加状态机快速解析xml工具

---
+++ src/nut/util/txtcfg/xml/element_handler.h
@@ -0,0 +1,76 @@
+/**
+ * @file -
+ * @author jingqi
+ * @date 2014-09-02
+ * @last-edit 2014-09-02 23:57:49 jingqi
+ * @brief
+ */
+
+#ifndef ___HEADFILE_543264C6_1906_4757_AFBF_6B9D023B2EA6_
+#define ___HEADFILE_543264C6_1906_4757_AFBF_6B9D023B2EA6_
+
+#include <string>
+
+namespace nut
+{
+
+/**
+ * 读取 xml 时处理各个 XML element 的 handler
+ */
+class XmlElementHandler
+{
+public:
+    enum HANDLE_MASK
+    {
+        HANDLE_ATTRIBUTE = 0x01,
+        HANDLE_TEXT = 0x02,
+        HANDLE_COMMENT = 0x04,
+        HANDLE_CHILD = 0x08
+    };
+
+    const uint8_t handle_mask;
+
+    XmlElementHandler(uint8_t mask = 0xFF)
+        : handle_mask(mask)
+    {}
+
+    virtual ~XmlElementHandler()
+    {}
+
+    virtual void handle_attribute(const std::string& name, const std::string& value)
+    {
+        (void) name;
+        (void) value;
+    }
+
+    virtual void handle_text(const std::string& text)
+    {
+        (void) text;
+    }
+
+    virtual void handle_comment(const std::string& comment)
+    {
+        (void) comment;
+    }
+
+    // 返回新的 handler
+    virtual XmlElementHandler* handle_child(const std::string& name)
+    {
+        (void) name;
+        return NULL;
+    }
+
+    // 回收旧的 handler
+    virtual void handle_child_finish(XmlElementHandler *child)
+    {
+        (void) child;
+    }
+
+    virtual void handle_finish()
+    {}
+
+};
+
+}
+
+#endif
---
+++ test/util/txtcfg/xml/test_xml_parser.cpp
@@ -0,0 +1,94 @@
+
+#include <nut/unittest/unittest.hpp>
+
+#include <nut/util/txtcfg/xml/xml_parser.hpp>
+
+#include <iostream>
+
+using namespace std;
+using namespace nut;
+
+namespace
+{
+
+string tag;
+
+class Handle : public XmlElementHandler
+{
+    
+public:
+    virtual void handle_attribute(const std::string &name, const std::string &value)
+    {
+        printf("attrib %s=%s\n", name.data(), value.data());
+        if (name == "a" && value == "b&\"<>")
+            tag += "a";
+    }
+    
+    virtual void handle_text(const std::string& text)
+    {
+        printf("text %s\n", text.data());
+        if (text == "c")
+            tag += "t";
+    }
+    
+    virtual void handle_comment(const std::string& comment)
+    {
+        printf("comment %s\n", comment.data());
+        if (comment == "d")
+            tag += "c";
+    }
+    
+    virtual XmlElementHandler* handle_child(const std::string &name)
+    {
+        printf("child %s\n", name.data());
+        if (name == "ch")
+            tag += "h";
+        return this;
+    }
+    
+    virtual void handle_child_finish(XmlElementHandler *child)
+    {
+        printf("fin child\n");
+        if (child == this)
+            tag += "x";
+    }
+    
+    virtual void handle_finish()
+    {
+        printf("fin\n");
+        tag += "y";
+    }
+};
+    
+}
+
+NUT_FIXTURE(TestXmlParser)
+{
+    NUT_CASES_BEGIN()
+    NUT_CASE(testSmoke)
+    NUT_CASES_END()
+
+    void setUp()
+    {
+        tag.clear();
+    }
+
+    void tearDown() {}
+    
+    void testSmoke()
+    {
+        Handle h;
+        XmlParser p(&h);
+        const char *s =
+        "<a><ch a=\"b&amp;&quot;&lt;&gt;\"/>c<!--d--></a>";
+        p.input(s);
+        p.finish();
+        if (p.has_error())
+            printf("xml error %d:%d %s\n", p.line(), p.column(), p.error_message().data());
+        NUT_TA(!p.has_error());
+        printf("%s\n", tag.data());
+        NUT_TA(tag == "hayxtcyx");
+    }
+};
+
+NUT_REGISTER_FIXTURE(TestXmlParser, "util, txtcfg, quiet");
