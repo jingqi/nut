351e176950ca02fa66812a685758b5068986c954
jingqi <jingqi.se@gmail.com>
2015-01-23 21:58:44 +0800

    使用中间计算值全为正数的 karatsuba 算法，性能获得优化

--- src/nut/numeric/word_array_integer.hpp
+++ src/nut/numeric/word_array_integer.hpp
@@ -1598,27 +1598,23 @@ void unsigned_multiply(const T *a, size_t M, const T *b, size_t N, T *x, size_t 
     }
 }
 
+template <typename T, typename MemAlloc>
+void unsigned_karatsuba_multiply(const T *a, size_t M, const T *b, size_t N, T *x, size_t P, MemAlloc *ma = NULL);
+
 /**
- * karatsuba 乘法，时间复杂度为 O(n^1.59)
+ * karatsuba 乘法，时间复杂度为 O(n^(log2 3)) = O(n^1.58)
  *
  * a = A * base + B (其中 base = 2 ^ (n/2) )
  * b = C * base + D
- * a * b = AC * (base^2) + ((A-B)(D-C) + AC + BD) * base + BD
- * 只需要做 3 次 n/2 规模的乘法，以及一些加减法
+ * a * b = AC * (base^2) + ((A+B)(C+D) - AC - BD) * base + BD
+ * 该算式中乘法总体规模变小了
  */
 template <typename T, typename MemAlloc>
 void signed_karatsuba_multiply(const T *a, size_t M, const T *b, size_t N, T *x, size_t P, MemAlloc *ma = NULL)
 {
     assert(NULL != a && M > 0 && NULL != b && N > 0 && NULL != x && P > 0);
 
-    // 规模较小时使用一般算法
-    if (M < 5 || N < 5 || P < 5)
-    {
-        signed_multiply(a, M, b, N, x, P, ma);
-        return;
-    }
-
-    // 改算法不能处理负数的补数形式
+    // karatsuba 算法不能处理负数的补数形式
     T *aa = const_cast<T*>(a), *bb = const_cast<T*>(b);
     size_t MM = M, NN = N;
     bool neg = false;
@@ -1637,89 +1633,123 @@ void signed_karatsuba_multiply(const T *a, size_t M, const T *b, size_t N, T *x,
         neg = !neg;
     }
 
+    // 调用 karatsuba 算法
+    unsigned_karatsuba_multiply(aa, MM, bb, NN, x, P, ma);
+    if (aa != a)
+        ma_free(ma, aa);
+    if (bb != b)
+        ma_free(ma, bb);
+
+    // 还原符号
+    if (neg)
+        signed_negate(x, P, x, P, ma);
+}
+
+/**
+ * karatsuba 乘法，时间复杂度为 O(n^(log2 3)) = O(n^1.58)
+ *
+ * a = A * base + B (其中 base = 2 ^ (n/2) )
+ * b = C * base + D
+ * a * b = AC * (base^2) + ((A+B)(C+D) - AC - BD) * base + BD
+ * 该算式中乘法总体规模变小了
+ */
+template <typename T, typename MemAlloc>
+void unsigned_karatsuba_multiply(const T *a, size_t M, const T *b, size_t N, T *x, size_t P, MemAlloc *ma = NULL)
+{
+    assert(NULL != a && M > 0 && NULL != b && N > 0 && NULL != x && P > 0);
+
+    // 去除无效位长
+    M = (std::min)(unsigned_significant_size(a, M), P);
+    N = (std::min)(unsigned_significant_size(b, N), P);
+
+    // 规模较小时使用一般算法
+    if (M < 5 || N < 5 || P < 5)
+    {
+        unsigned_multiply(a, M, b, N, x, P, ma);
+        return;
+    }
+
     // 准备变量 A、B、C、D
-    const size_t base_len = ((std::max)(MM, NN) + 1) / 2;
+    const size_t base_len = ((std::max)(M, N) + 1) / 2;
     const T ZERO = 0;
-    const T *A = aa + base_len;
-    size_t a_len = MM - base_len;
-    if (MM <= base_len)
+
+    // a_len = max(0, min(M - base_len, P - base_len))
+    //       = max(0, M - base_len)
+    // since, M <= P
+    const T *A = a + base_len;
+    size_t a_len = M - base_len;
+    if (M <= base_len)
     {
         A = &ZERO;
         a_len = 1;
     }
 
-    T *B = const_cast<T*>(aa);
-    size_t b_len = (std::min)(MM, base_len);
-    if (!is_positive(B, b_len))
-    {
-        B = (T*) ma_alloc(ma, sizeof(T) * (b_len + 1));
-        ::memcpy(B, aa, sizeof(T) * b_len);
-        B[b_len] = 0;
-        ++b_len;
-    }
+    // b_len = min(base_len, M)
+    const T *B = a;
+    const size_t b_len = (std::min)(base_len, M);
 
-    const T *C = bb + base_len;
-    size_t c_len = NN - base_len;
+    // c_len = max(0, min(N - base_len, P - base_len))
+    //       = max(0, N - base_len)
+    // since, N <= P
+    const T *C = b + base_len;
+    size_t c_len = N - base_len;
     if (N <= base_len)
     {
         C = &ZERO;
         c_len = 1;
     }
 
-    T *D = const_cast<T*>(bb);
-    size_t d_len = (std::min)(NN, base_len);
-    if (!is_positive(D, d_len))
-    {
-        D = (T*) ma_alloc(ma, sizeof(T) * (d_len + 1));
-        ::memcpy(D, bb, sizeof(T) * d_len);
-        D[d_len] = 0;
-        ++d_len;
-    }
+    // d_len = min(base_len, N)
+    const T *D = b;
+    size_t d_len = (std::min)(base_len, N);
 
-    T *AC = (T*) ma_alloc(ma, sizeof(T) * (base_len * 2));
-    signed_karatsuba_multiply(A, a_len, C, c_len, AC, base_len * 2, ma);
+    // 计算中间结果
 
-    T *BD = (T*) ma_alloc(ma, sizeof(T) * (base_len * 2 + 1));
-    signed_karatsuba_multiply(B, b_len, D, d_len, BD, base_len * 2 + 1, ma);
+    // ac_len = min(a_len + c_len, max(0, P - base_len))
+    //        = min(a_len + c_len, P - base_len)
+    // since, P - base_len >= max(M,N) - base_len > 0
+    const size_t ac_len = (std::min)(a_len + c_len, P - base_len);
+    T *AC = (T*) ma_alloc(ma, sizeof(T) * ac_len);
+    unsigned_karatsuba_multiply(A, a_len, C, c_len, AC, ac_len, ma);
 
-    T *D_C = (T*) ma_alloc(ma, sizeof(T) * (base_len + 1));
-    signed_sub(D, d_len, C, c_len, D_C, base_len + 1, ma);
-    if (D != bb)
-        ma_free(ma, D);
-    D = NULL;
-    if (bb != b)
-        ma_free(ma, bb);
-    bb = NULL;
+    // bd_len = min(b_len + d_len, P)
+    const size_t bd_len = (std::min)(b_len + d_len, P);
+    T *BD = x;
+    unsigned_karatsuba_multiply(B, b_len, D, d_len, BD, bd_len, ma);
 
-    T *ABCD = (T*) ma_alloc(ma, sizeof(T) * (base_len * 2 + 1));
-    signed_sub(A, a_len, B, b_len, ABCD, base_len + 1, ma);
-    if (B != aa)
-        ma_free(ma, B);
-    B = NULL;
-    if (aa != a)
-        ma_free(ma, aa);
-    aa = NULL;
+    // ab_len = min(max(a_len, b_len) + 1, P - base_len)
+    //        = min(b_len + 1, P - base_len)
+    // since, a_len <= b_len
+    const size_t ab_len = (std::min)(b_len + 1, P - base_len);
+    T *AB = (T*) ma_alloc(ma, sizeof(T) * ab_len);
+    unsigned_add(A, a_len, B, b_len, AB, ab_len, ma);
 
-    signed_karatsuba_multiply(ABCD, base_len + 1, D_C, base_len + 1, ABCD, base_len * 2 + 1, ma);
-    ma_free(ma, D_C);
-    D_C = NULL;
+    // cd_len = min(max(c_len, d_len) + 1, P - base_len)
+    //        = min(d_len + 1, P - base_len)
+    // since, c_len <= d_len
+    const size_t cd_len = (std::min)(d_len + 1, P - base_len);
+    T *CD = (T*) ma_alloc(ma, sizeof(T) * cd_len);
+    unsigned_add(C, c_len, D, d_len, CD, cd_len, ma);
 
-    signed_add(ABCD, base_len * 2 + 1, AC, base_len * 2, ABCD, base_len * 2 + 1, ma);
-    signed_add(ABCD, base_len * 2 + 1, BD, base_len * 2 + 1, ABCD, base_len * 2 + 1, ma);
+    // abcd_len = min(ab_len + cd_len, P - base_len)
+    //          = min(b_len + d_len + 1, P - base_len)
+    const size_t abcd_len = (std::min)(b_len + d_len + 1, P - base_len);
+    T *ABCD = (T*) ma_alloc(ma, sizeof(T) * abcd_len);
+    unsigned_karatsuba_multiply(AB, ab_len, CD, cd_len, ABCD, abcd_len, ma);
+    ma_free(ma, AB);
+    ma_free(ma, CD);
 
-    // 并生成结果
-    signed_expand(BD, base_len * 2 + 1, x, P);
-    if (P > base_len)
-        signed_add(x + base_len, P - base_len, ABCD, base_len * 2 + 1, x + base_len, P - base_len, ma);
-    if (P > base_len * 2)
-        signed_add(x + base_len * 2, P - base_len * 2, AC, base_len * 2, x + base_len * 2, P - base_len * 2, ma);
-    if (neg)
-        signed_negate(x, P, x, P, ma);
+    unsigned_sub(ABCD, abcd_len, AC, ac_len, ABCD, abcd_len, ma);
+    unsigned_sub(ABCD, abcd_len, BD, bd_len, ABCD, abcd_len, ma);
 
-    // 回收内存
-    ma_free(ma, AC);
-    ma_free(ma, BD);
+    // 生成最终结果
+    if (bd_len < P)
+        ::memset(x + bd_len, 0, sizeof(T) * (P - bd_len));
+    unsigned_add(x + base_len, P - base_len, ABCD, abcd_len, x + base_len, P - base_len, ma);
     ma_free(ma, ABCD);
+    if (P > base_len * 2)
+        unsigned_add(x + base_len * 2, P - base_len * 2, AC, ac_len, x + base_len * 2, P - base_len * 2, ma);
+    ma_free(ma, AC);
 }
 
 /**
