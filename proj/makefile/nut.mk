c29c68f052c759b7bbea09edf9927a41ee4f4607
jingqi <jingqi.se@gmail.com>
2015-08-11 01:37:12 +0800

    makefile 编译中间产物单独放到临时文件；编译依赖于 makefile 自身；makefile 自动提前生成所需的目录树

--- proj/makefile/nut.mk
+++ proj/makefile/nut.mk
@@ -4,56 +4,73 @@ CC = g++
 LD = gcc
 AR = ar
 
-HOST = $(shell uname -s)
+# variables
+SRC_ROOT = ../../src/nut
+OBJ_ROOT = obj/nut
+THIS = nut.mk
 
 # INC
 INC += -I../../src
 
 # LIB
-ifeq ($(HOST), Darwin)
+HOST = $(shell uname -s)
+ifeq (${HOST}, Darwin)
 	LIB += -lstdc++
 else
 	LIB += -lpthread
 endif
 
 # CC_FLAGS
-ifneq ($(HOST), Darwin)
+ifneq (${HOST}, Darwin)
 	CC_FLAGS += -rdynamic
 endif
 CC_FLAGS += -Wall -g -std=c++11
 
-PWD = $(shell pwd)
-DIRS = $(shell find ../../src/nut -maxdepth 10 -type d)
-CPPS = $(foreach dir,$(DIRS),$(wildcard $(dir)/*.cpp))
-OBJS = $(patsubst %.cpp,%.o,$(CPPS))
-DEPS = $(patsubst %.cpp,%.d,$(CPPS))
+# OBJS, DEPS
+DIRS = $(shell find ${SRC_ROOT} -maxdepth 10 -type d)
+CPPS = $(foreach dir,${DIRS},$(wildcard $(dir)/*.cpp))
+OBJS = $(patsubst ${SRC_ROOT}%.cpp,${OBJ_ROOT}%.o,${CPPS})
+DEPS = ${OBJS:.o=.d}
 
-TARGET = $(PWD)/libnut.a
+# TARGET
+TARGET = $(CURDIR)/libnut.a
 
-all: $(TARGET)
+# mkdirs
+$(shell mkdir -p $(patsubst ${SRC_ROOT}%,${OBJ_ROOT}%,${DIRS}))
+
+.PHONY: all clean rebuild dirs
+
+all: dirs ${TARGET}
 
 clean:
-	rm -rf $(OBJS)
-	rm -rf $(DEPS)
-	rm -rf $(TARGET)
+	rm -rf ${OBJS}
+	rm -rf ${DEPS}
+	rm -rf ${TARGET}
 
 rebuild: clean all
 
-$(TARGET): $(OBJS)
-	$(AR) cqs $@ $^
+dirs:
+	@mkdir -p $(patsubst ${SRC_ROOT}%,${OBJ_ROOT}%,${DIRS})
 
-%.o: %.cpp
-	$(CC) $(INC) $(DEF) $(CC_FLAGS) -c $< -o $@
+$(TARGET): ${OBJS} ${THIS}
+	$(AR) cqs $@ ${OBJS}
+
+${OBJ_ROOT}/%.o: ${SRC_ROOT}/%.cpp
+	$(CC) ${INC} ${DEF} ${CC_FLAGS} -c $< -o $@
 
 ## 动态生成依赖关系
-%.d: %.cpp
+# %.d: %.cpp
+${OBJ_ROOT}/%.d:${SRC_ROOT}/%.cpp
 	@rm -f $@
 	@# 向 *.d 中写入 "xx/xx/*.o xx/xx/*.d:\" 这样一个字符串
-	@echo '$< $@:\\' | sed 's/[.]cpp/.o/g' >$@
+	@echo '$@ $@.o:\\' | sed 's/[.]d[.]o/.o/g' > $@
+	@# 向 *.d 中写入 makefile 依赖
+	@echo '${THIS} \\' >> $@
 	@# 向 *.d.$ 中写入用 gcc -MM 生成的初始依赖关系
-	$(CC) $(INC) $(DEF) -MM $< >$@.$$
+	$(CC) ${INC} ${DEF} -MM $< > $@.$$
 	@# 将 *.d.$ 中内容去除冒号前的内容，剩余内容写入 *.d 中
-	@sed 's/^.*[:]//g' <$@.$$ >>$@
+	@sed 's/^.*[:]//g' < $@.$$ >> $@
+	@echo '${THIS}:' >> $@
 	@# 对 *.d.$ 的内容以此处理：
 	@#	sed 去除冒号前的内容
 	@#	sed 去除续行符
@@ -66,4 +83,4 @@ $(TARGET): $(OBJS)
 
 # 引入动态依赖关系
 #	起首的'-'符号表示忽略错误命令(这里忽略不存在的文件，不再打warning)
--include ${OBJS:.o=.d}
+-include ${DEPS}
--- proj/makefile/test_nut.mk
+++ proj/makefile/test_nut.mk
@@ -4,76 +4,92 @@ CC = g++
 LD = gcc
 AR = ar
 
-HOST = $(shell uname -s)
+# variables
+SRC_ROOT = ../../src/test_nut
+OBJ_ROOT = obj/test_nut
+THIS = test_nut.mk
 
 # INC
-INC += -I../../src
+INC += -I${SRC_ROOT}/..
 
 # LIB
 LIB_DEPS = ./libnut.a
-ifeq ($(HOST), Darwin)
+HOST = $(shell uname -s)
+ifeq (${HOST}, Darwin)
 	LIB += -lstdc++
 else
 	LIB += -lpthread -lrt
 endif
 
 # CC_FLAGS
-ifneq ($(HOST), Darwin)
+ifneq (${HOST}, Darwin)
 	CC_FLAGS = -rdynamic
 endif
 CC_FLAGS += -Wall -g -std=c++11
 
-PWD = $(shell pwd)
-DIRS = $(shell find ../../src/test_nut -maxdepth 10 -type d)
-CPPS = $(foreach dir,$(DIRS),$(wildcard $(dir)/*.cpp))
-OBJS = $(patsubst %.cpp,%.o,$(CPPS))
-DEPS = $(patsubst %.cpp,%.d,$(CPPS))
+# OBJS, DEPS
+DIRS = $(shell find ${SRC_ROOT} -maxdepth 10 -type d)
+CPPS = $(foreach dir,${DIRS},$(wildcard $(dir)/*.cpp))
+OBJS = $(patsubst ${SRC_ROOT}%.cpp,${OBJ_ROOT}%.o,$(CPPS))
+DEPS = ${OBJS:.o=.d}
 
-TARGET = $(PWD)/test_nut
+# TARGET
+TARGET = $(CURDIR)/test_nut
 
-all: $(TARGET)
+# mkdirs
+$(shell mkdir -p $(patsubst ${SRC_ROOT}%,${OBJ_ROOT}%,${DIRS}))
+
+.PHONY: all clean rebuild run gdb cgdb nemiver valgrind dirs
+
+all: dirs ${TARGET}
 
 clean:
-	rm -rf $(OBJS)
-	rm -rf $(DEPS)
-	rm -rf $(TARGET)
+	rm -rf ${OBJS}
+	rm -rf ${DEPS}
+	rm -rf ${TARGET}
 
 rebuild: clean all
 
-run: $(TARGET)
-	$(TARGET)
+run: ${TARGET}
+	${TARGET}
 
-gdb: $(TARGET)
-	gdb $(TARGET)
+gdb: ${TARGET}
+	gdb ${TARGET}
 
-cgdb: $(TARGET)
-	cgdb $(TARGET)
+cgdb: ${TARGET}
+	cgdb ${TARGET}
 
-nemiver: $(TARGET)
-	nemiver $(TARGET)
+nemiver: ${TARGET}
+	nemiver ${TARGET}
 
-valgrind: $(TARGET)
-	valgrind -v --leak-check=full $(TARGET)
+valgrind: ${TARGET}
+	valgrind -v --leak-check=full ${TARGET}
+
+dirs:
+	@mkdir -p $(patsubst ${SRC_ROOT}%,${OBJ_ROOT}%,${DIRS})
 
 ./libnut.a:
 	make -f nut.mk
 
 # NOTE: in linux, $(LIB) should be the last parameter
-$(TARGET): $(OBJS) $(LIB_DEPS)
-	$(CC) -o $@ $^ $(LIB)
+$(TARGET): ${OBJS} ${LIB_DEPS} ${THIS}
+	$(CC) -o $@ ${OBJS} ${LIB_DEPS} $(LIB)
 
-%.o: %.cpp
-	$(CC) $(INC) $(DEF) $(CC_FLAGS) -c $< -o $@
+${OBJ_ROOT}/%.o: ${SRC_ROOT}/%.cpp
+	$(CC) ${INC} ${DEF} ${CC_FLAGS} -c $< -o $@
 
 ## 动态生成依赖关系
-%.d: %.cpp
+${OBJ_ROOT}/%.d: ${SRC_ROOT}/%.cpp
 	@rm -f $@
 	@# 向 *.d 中写入 "xx/xx/*.o xx/xx/*.d:\" 这样一个字符串
-	@echo '$< $@:\\' | sed 's/[.]cpp/.o/g' >$@
+	@echo '$@ $@.o:\\' | sed 's/[.]d[.]o/.o/g' > $@
+	@# 向 *.d 中写入 makefile 依赖
+	@echo '${THIS} \\' >> $@
 	@# 向 *.d.$ 中写入用 gcc -MM 生成的初始依赖关系
-	$(CC) $(INC) $(DEF) -MM $< >$@.$$
+	$(CC) ${INC} ${DEF} -MM $< > $@.$$
 	@# 将 *.d.$ 中内容去除冒号前的内容，剩余内容写入 *.d 中
-	@sed 's/^.*[:]//g' <$@.$$ >>$@
+	@sed 's/^.*[:]//g' < $@.$$ >> $@
+	@echo '${THIS}:' >> $@
 	@# 对 *.d.$ 的内容以此处理：
 	@#	sed 去除冒号前的内容
 	@#	sed 去除续行符
@@ -86,4 +102,4 @@ $(TARGET): $(OBJS) $(LIB_DEPS)
 
 # 引入动态依赖关系
 #	起首的'-'符号表示忽略错误命令(这里忽略不存在的文件，不再打warning)
--include ${OBJS:.o=.d}
+-include ${DEPS}
