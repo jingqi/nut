c0e2d27727d6e5cc9f53ebdf4dceb0b6e3eff6d9
jingqi <jingqi.se@gmail.com>
2016-11-28 10:35:51 +0800

    修正 vs2015 项目文件

--- src/test_nut/util/string/test_stringutil.cpp
+++ src/test_nut/util/string/test_stringutil.cpp
@@ -51,16 +51,16 @@ NUT_FIXTURE(TestStringUtil)
 
         NUT_TA(format("%d,%s,%c,%f", 1, "am", 's', 1.23) == "1,am,s,1.230000");
 
-        //std::wstring wtmp;
+        // std::wstring wtmp;
         // wcout << format(L"%d,%S,%c,%f", 1, L"am", L's', 1.23) << endl;
 #if NUT_PLATFORM_OS_LINUX || NUT_PLATFORM_OS_MAC
-        //format(&wtmp, L"%d,%S,%C,%f", 1, L"am", L's', 1.23);
+        //wtmp = format(L"%d,%S,%C,%f", 1, L"am", L's', 1.23);
         //NUT_TA(wtmp == L"1,am,s,1.230000");
 
         NUT_TA(format(L"%d,%S,%C,%f", 1, L"am", L's', 1.23) == L"1,am,s,1.230000");
 #else
-        format(&wtmp, L"%d,%s,%c,%f", 1, L"am", L's', 1.23);
-        NUT_TA(wtmp == L"1,am,s,1.230000");
+        // wtmp = format(L"%d,%s,%c,%f", 1, L"am", L's', 1.23);
+        // NUT_TA(wtmp == L"1,am,s,1.230000");
 
         NUT_TA(format(L"%d,%s,%c,%f", 1, L"am", L's', 1.23) == L"1,am,s,1.230000");
 #endif
