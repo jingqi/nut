409e9ab0c61926b48cf60728c76230cfe74e27f8
jingqi <jingqi.se@gmail.com>
2017-01-06 16:54:22 +0800

    添加一个 nut.h 导出全部接口

---
+++ src/nut/nut.h
@@ -0,0 +1,132 @@
+﻿
+#ifndef ___HEADFILE_F97FB9D7_3968_448C_A782_47F67875098C_
+#define ___HEADFILE_F97FB9D7_3968_448C_A782_47F67875098C_
+
+#include "nut_config.h"
+
+// platform
+#include "platform/platform.h"
+#include "platform/stdint_traits.h"
+#include "platform/os.h"
+#include "platform/path.h"
+
+// debugging
+#include "debugging/exception.h"
+#include "debugging/destroy_checker.h"
+#include "debugging/source_location.h"
+#include "debugging/addr_map_mgr.h"
+#include "debugging/backtrace.h"
+
+// mem
+#include "mem/memory_allocator.h"
+#include "mem/sys_ma.h"
+#include "mem/lengthfixed_mp.h"
+#include "mem/segments_mp.h"
+#include "mem/scoped_gc.h"
+
+// memtool
+#include "memtool/singleton.h"
+#include "memtool/free_guard.h"
+
+// rc
+#include "rc/rc_ptr.h"
+#include "rc/rc_new.h"
+
+// container
+#include "container/array.h"
+#include "container/tuple.h"
+#include "container/bundle.h"
+#include "container/integer_set.h"
+#include "container/bit_stream.h"
+#include "container/lru_cache.h"
+#include "container/lru_data_cache.h"
+#include "container/bytestream/input_stream.h"
+#include "container/bytestream/output_stream.h"
+#include "container/bytestream/random_access_input_stream.h"
+#include "container/bytestream/byte_array_stream.h"
+#include "container/rwbuffer/ring_buffer.h"
+#include "container/rwbuffer/fragment_buffer.h"
+#include "container/skiplist/skiplist.h"
+#include "container/skiplist/skiplist_set.h"
+#include "container/skiplist/skiplist_map.h"
+#include "container/tree/btree.h"
+#include "container/tree/bstree.h"
+#include "container/tree/rbtree.h"
+#include "container/tree/rtree/mdarea.h"
+#include "container/tree/rtree/rtree.h"
+
+// numeric
+#include "numeric/word_array_integer.h"
+#include "numeric/big_integer.h"
+#include "numeric/numeric_algo/bit_sieve.h"
+#include "numeric/numeric_algo/gcd.h"
+#include "numeric/numeric_algo/mod.h"
+#include "numeric/numeric_algo/prime.h"
+#include "numeric/numeric_algo/karatsuba.h"
+
+// security
+#include "security/digest/adler32.h"
+#include "security/digest/md5.h"
+#include "security/digest/sha1.h"
+#include "security/encrypt/rsa.h"
+#include "security/encrypt/aes.h"
+#include "security/encrypt/aes_cbc_pkcs5.h"
+
+// threading
+#include "threading/threading.h"
+#include "threading/thread.h"
+#include "threading/thread_pool.h"
+#include "threading/lockfree/atomic.h"
+#include "threading/lockfree/stamped_ptr.h"
+#include "threading/lockfree/concurrent_stack.h"
+#include "threading/lockfree/concurrent_queue.h"
+#include "threading/sync/mutex.h"
+#include "threading/sync/spinlock.h"
+#include "threading/sync/rwlock.h"
+#include "threading/sync/sem.h"
+#include "threading/sync/condition.h"
+#include "threading/sync/guard.h"
+
+// unittest
+#include "unittest/unit_test.h"
+
+// pconsole
+#include "pconsole/pconsole.h"
+
+// logging
+#include "logging/logger.h"
+#include "logging/log_handler/log_handler.h"
+#include "logging/log_handler/stream_log_handler.h"
+#include "logging/log_handler/console_log_handler.h"
+#include "logging/log_handler/file_log_handler.h"
+#include "logging/log_handler/syslog_log_handler.h"
+#include "logging/log_handler/circle_file_by_time_log_handler.h"
+#include "logging/log_handler/circle_file_by_size_log_handler.h"
+
+// util
+#include "util/delegate.h"
+#include "util/console_util.h"
+#include "util/string/kmp.h"
+#include "util/string/to_string.h"
+#include "util/string/string_util.h"
+#include "util/time/date_time.h"
+#include "util/time/time_val.h"
+#include "util/time/timer_manager.h"
+#include "util/time/time_wheel.h"
+#include "util/txtcfg/txt_file.h"
+#include "util/txtcfg/property_dom.h"
+#include "util/txtcfg/ini_dom.h"
+#include "util/txtcfg/xml/element_handler.h"
+#include "util/txtcfg/xml/xml_element.h"
+#include "util/txtcfg/xml/xml_dom.h"
+#include "util/txtcfg/xml/xml_parser.h"
+#include "util/txtcfg/xml/xml_writer.h"
+
+#if defined(NUT_ENABLE_SQLITE3)
+#   include "util/sqlite_helper/sqlite_param.h"
+#   include "util/sqlite_helper/sqlite_result_set.h"
+#   include "util/sqlite_helper/sqlite_statement.h"
+#   include "util/sqlite_helper/sqlite_connection.h"
+#endif
+
+#endif
